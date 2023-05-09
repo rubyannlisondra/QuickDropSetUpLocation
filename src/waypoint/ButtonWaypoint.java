55e13062ee581a8ef761a8fba56a56db33494d8f
Ruby Ann <rubyannlisondra2@gmail.com>
2023-05-09 10:28:14 +0800

    MapIntegration

---
+++ src/waypoint/ButtonWaypoint.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package waypoint;
+
+import java.awt.Cursor;
+import java.awt.Dimension;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Ruby Ann
+ */
+public class ButtonWaypoint extends JButton {
+    public ButtonWaypoint() {
+        setContentAreaFilled(false);
+        setIcon(new ImageIcon(getClass().getResource("/icon/pin.png")));
+        setCursor(new Cursor(Cursor.HAND_CURSOR));
+        setSize(new Dimension(24, 24));
+    }
+    
+}
